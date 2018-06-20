003318b3449be82089613f4a978e5197c51a02a4
Joan95 <joancanals95@gmail.com>
2018-06-20 12:59:20 +0200

    Monitoring.sh started

--- monitoring.sh
+++ monitoring.sh
@@ -1,12 +1,26 @@
 #!/bin/bash 
 
-#ARGS: (logsFile)
+#ARGS: (logsFile, fileRunning)
 #WHERE 	logsFile		= $directoryLogsPath/$nameLogsFile
 #WHERE 	directoryLogsPath	= $pathToLogs/$nameFolder
 #WHERE 	pathToLogs 		= $base_directory/logs
+#WHERE 	fileRunning 		= whether encrypter.sh or decrypter.sh
 
-echo "Encrypt" >> /home/pi/Desktop/TFG/Logs/outputEncrypt.txt
-$(top -bn40 -d 0.2 | grep encrypter.sh) >> /home/pi/Desktop/TFG/Logs/outputEncrypt.txt
+echo -e "\n\n\tInside de 'monitoring.sh'"
+
+#MONITORING METHODS:
+	#CPU: top -bn1
+	#DISK: df -h
+	#MEMORY: free -m
+
+#for i in "$@"; do
+	#echo "$i"
+#done
+
+logsFile=$(echo "$1")
+fileRunning=$(echo "$2")
+
+$(top -bn40 -d 0.2 | grep root | grep $fileRunning) >> $logsFile
 
 
 exit 0
